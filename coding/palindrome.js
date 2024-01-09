24b07a9b11b5af3a2c4c1611939bee5ded60b3e0
rizwan-muhd <rizwankk448@gmail.com>
2024-01-09 13:29:36 +0530

    palindrome code

--- coding/palindrome.js
+++ coding/palindrome.js
@@ -1,1 +1,11 @@
-const palindrome = () => {};
+const palindrome = (str) => {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].toLowerCase() !== str[str.length - 1 - i].toLowerCase()) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const res = palindrome("helleH");
+console.log(res);
