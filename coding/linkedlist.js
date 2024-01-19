7139ccd2aa5694b9bf331f0916dbfdef3d4094b9
rizwan-muhd <rizwankk448@gmail.com>
2024-01-20 00:05:10 +0530

    linked list problems

--- coding/linkedlist.js
+++ coding/linkedlist.js
@@ -9,4 +9,29 @@ class LinkedList {
   constructor(head = null) {
     this.head = head;
   }
+
+  // insert data
+
+  insertData(data) {
+    const newNode = new Node(data);
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+
+  display() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data + " -> ");
+      current = current.next;
+    }
+    console.log("null");
+  }
 }
+
+const linkedList = new LinkedList();
+
+linkedList.insertData(5);
+linkedList.insertData(6);
+linkedList.insertData(8);
+
+linkedList.display();
